f09dbfc6fcbd7639ed7022b6a96e0f2f04000e5b
Yu Moriuchi <ykdr642@gmail.com>
2018-10-23 02:27:42 +0900

    complete template

--- main.py
+++ main.py
@@ -1,68 +1,25 @@
 import pytest
 from testTool import Drive
 
-def get_field(input_stdin):
+def get_field(input_stdin,Number=False):
     out = input_stdin.split()
     if len(out) > 1:
-        return out
+        if Number:
+            try:
+                return list(map(lambda x:int(x), out))
+            except:
+                return out
+        else:
+            return out
     else:
-        return out[0]
-
-class game:
-    def __init__(self,field_inf):
-        field = get_field(field_inf)
-        self.hight = int(field[0])
-        self.width = int(field[1])
-        self.people = int(field[2])
-        self.card_list = []
-        self.score_board = []
-        for hoge in range(self.people):
-            self.score_board.append(0)
-        self.player = 0
-
-    def get_card(self,stdin):
-        input = get_field(stdin)
-        self.card_list.append(input)
-
-    def add_score(self):
-        self.score_board[self.player] += 2
-
-    def next(self):
-        self.player += 1
-        if self.player == self.people:
-            self.player = 0
-
-    def check_card(self,stdin):
-        input_list = get_field(stdin)
-        hw = list(map(lambda x: int(x) - 1,input_list))
-        if self.card_list[hw[0]][hw[1]] == self.card_list[hw[2]][hw[3]] and \
-            self.card_list[hw[0]][hw[1]] is not None and \
-            not (hw[0] == hw[2] and hw[1] == hw[3]):
-            self.card_list[hw[0]][hw[1]] = None
-            self.card_list[hw[2]][hw[3]] = None
-            return True
+        if Number:
+            try:
+                return int(out[0])
+            except:
+                return out[0]
         else:
-            return False
-
-    def turn(self,stdin):
-        judge = self.check_card(stdin)
-        if judge:
-            self.add_score()
-        else:
-            self.next()
-
-    def output(self):
-        for _score in self.score_board:
-            print(_score)
-    
-
+            return out[0]
 
 class testProgram(Drive):
     def test(self):
-        game1 = game(self.input())
-        for num in range(game1.hight):
-            game1.get_card(self.input())
-        num = int(self.input())
-        for i in range(num):
-            game1.turn(self.input())
-        game1.output()
+        print(self.input())
--- test_paiza.py
+++ test_paiza.py
@@ -1,28 +1,40 @@
 import main
 import pytest
-"""
-detabase = [
-    (["2 2 2","1 2","2 1","2","1 2 2 1","1 1 2 2"],"4\n0\n"),
-    (["2 4 4","1 2 1 3","2 1 1 3","6","1 2 2 1","1 3 1 4","2 1 2 3","1 4 2 4","1 1 1 3","2 2 2 3"],"2\n0\n6\n0\n")
-]
-"""
+
 inputMessage = "testCase,expected"
 testcase = []
 
-#@pytest.fixture()
 def getTestCase():
     f = open("testcase.txt","r")
-    testcase = f.readlines()
+    testCaseList = f.readlines()
     f.close()
-    testcase = list(map(lambda x: x.replace("\n",""),testcase))
-    return testcase
+    tmp = []
+    test = []
+    for _case in testCaseList:
+        if _case is not "\n":
+            tmp.append(_case)
+        else:
+            test.append(tmp[:])
+            tmp = []
+    test.append(tmp[:])
+    testcase = test[0::2]
+    _tmpexpected = test[1::2]
+    testCase = []
+    for oneTestcase in testcase:
+        testCase.append((list(map(lambda x:x.replace("\n",""),oneTestcase))))
+    onexpected = ""
+    expected = []
+    for _etmp in _tmpexpected:
+        for t in _etmp:
+            onexpected += t
+        expected.append(onexpected)
+        onexpected = ""
+    detabase = []
+    for t,e in zip(testCase, expected):
+        detabase.append((t,e))
+    return detabase
 
-g = getTestCase()
-
-def makeCase():
-    return [(["2 2 2","1 2","2 1","2","1 2 2 1","1 1 2 2"],"4\n0\n"),(["2 4 4","1 2 1 3","2 1 1 3","6","1 2 2 1","1 3 1 4","2 1 2 3","1 4 2 4","1 1 1 3","2 2 2 3"],"2\n0\n6\n0\n")]
-
-detabase = makeCase()
+detabase = getTestCase()
 
 @pytest.mark.parametrize(inputMessage,detabase)
 def test_Paiza(testCase,expected,capsys):
@@ -30,6 +42,3 @@ def test_Paiza(testCase,expected,capsys):
     test_obj.test()
     out,err = capsys.readouterr()
     assert out == expected
-
-def test_1():
-    assert g == ["1 2 3","1 2","1","","12 13","1"]
