dfce8ff2611beebe1b147ea2140e8aaff3d83e4a
Yu Moriuchi <ykdr642@gmail.com>
2018-10-27 21:36:10 +0900

    move method

--- testTool.py
+++ testTool.py
@@ -12,3 +12,33 @@ class Drive:
 #main
     def test(self):
         pass
+
+def getTestCase():
+    f = open("testcase.txt","r")
+    testCaseList = f.readlines()
+    f.close()
+    tmp = []
+    test = []
+    for _case in testCaseList:
+        if _case is not "\n":
+            tmp.append(_case)
+        else:
+            test.append(tmp[:])
+            tmp = []
+    test.append(tmp[:])
+    testcase = test[0::2]
+    _tmpexpected = test[1::2]
+    testCase = []
+    for oneTestcase in testcase:
+        testCase.append((list(map(lambda x:x.replace("\n",""),oneTestcase))))
+    onexpected = ""
+    expected = []
+    for _etmp in _tmpexpected:
+        for t in _etmp:
+            onexpected += t
+        expected.append(onexpected)
+        onexpected = ""
+    detabase = []
+    for t,e in zip(testCase, expected):
+        detabase.append((t,e))
+    return detabase
--- test_paiza.py
+++ test_paiza.py
@@ -1,38 +1,9 @@
 import main
 import pytest
+from testTool import getTestCase
 
 inputMessage = "testCase,expected"
 testcase = []
-
-def getTestCase():
-    f = open("testcase.txt","r")
-    testCaseList = f.readlines()
-    f.close()
-    tmp = []
-    test = []
-    for _case in testCaseList:
-        if _case is not "\n":
-            tmp.append(_case)
-        else:
-            test.append(tmp[:])
-            tmp = []
-    test.append(tmp[:])
-    testcase = test[0::2]
-    _tmpexpected = test[1::2]
-    testCase = []
-    for oneTestcase in testcase:
-        testCase.append((list(map(lambda x:x.replace("\n",""),oneTestcase))))
-    onexpected = ""
-    expected = []
-    for _etmp in _tmpexpected:
-        for t in _etmp:
-            onexpected += t
-        expected.append(onexpected)
-        onexpected = ""
-    detabase = []
-    for t,e in zip(testCase, expected):
-        detabase.append((t,e))
-    return detabase
 
 detabase = getTestCase()
 
