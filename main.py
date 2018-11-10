d83fb8a86c5c564f27d529dd8571e1cc81ae3df2
Moriuchi Yu <ykdr642@gmail.com>
2018-11-10 14:27:05 +0900

    add makeMatrix

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pytest
 from testTool import Drive
 
 
+def makeMatrix(row:int,column:int,init):
+    r = [init for i in range(column)]
+    m = [r[:] for i in range(row)]
+    return m
+
+
 def getNumList(stdin):
     out = stdin.split()
     return list(map(lambda x:int(x), out))
